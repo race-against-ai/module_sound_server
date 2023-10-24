b4e0d46f4b051b803888701d64a1098bf3307761
Calvin Teuber <calvinteuber7@gmail.com>
2023-10-24 08:21:24 +0200

    Added support for special sounds and connection to live visu

--- sound_server_backend/sound_server.py
+++ sound_server_backend/sound_server.py
@@ -2,35 +2,54 @@ import os
 import pygame
 from pathlib import Path
 import pynng
+from random import choice
+from time import sleep
+
+REQUEST_ADDRESS = "ipc:///tmp/RAAI/sound_request.ipc"
+STATUS_ADDRESS = "ipc:///tmp/RAAI/sound_server_status.ipc"
+
+
+def get_sound_folder_content(folder):
+    sound_dict = {}
+    for file in os.listdir(folder):
+        if file.endswith('.mp3'):
+            sound_dict[file] = os.path.join(folder, file)
+    print(sound_dict)
+    return sound_dict
 
 
 class SoundPlayer:
-    def __init__(self, sound_folder):
+    def __init__(self, sound_folder, meme_sound_folder):
         self.sound_folder = sound_folder
-        self.sound_dict = self.get_sound_folder_content()
+        self.meme_sound_folder = meme_sound_folder
+        self.sound_dict = get_sound_folder_content(self.sound_folder)
+        self.meme_sound_dict = get_sound_folder_content(self.meme_sound_folder)
         pygame.init()
         pygame.mixer.init()
-
-    def get_sound_folder_content(self):
-        sound_dict = {}
-        for file in os.listdir(self.sound_folder):
-            sound_dict[file] = os.path.join(self.sound_folder, file)
-        print(sound_dict)
-        return sound_dict
 
     def play_sound(self, sound_file):
         sound = pygame.mixer.Sound(self.sound_dict.get(sound_file))
         if sound:
             sound.play()
-            pygame.time.delay(int(sound.get_length() * 100))
+            pygame.time.delay(int(sound.get_length() * 1000))
         else:
             print("Sound not found: ", sound_file)
 
+    def play_random_meme(self):
+        random_sound = choice(list(self.meme_sound_dict.keys()))
+        sound = pygame.mixer.Sound(self.meme_sound_dict.get(random_sound))
+        if sound:
+            sound.play()
+            pygame.time.delay(int(sound.get_length() * 1000))
+        else:
+            print("Sound not found: ", random_sound)
+
 
 class RequestHandler:
-    def __init__(self, sound_player, request_address):
+    def __init__(self, sound_player, request_address, status_socket):
         self.sound_player = sound_player
         self.request_address = request_address
+        self.status_socket = status_socket
 
     def run(self):
         with pynng.Rep0() as sock:
@@ -42,25 +61,35 @@ class RequestHandler:
                 self.process_request(msg, sock)
 
     def process_request(self, msg, sock):
-        if msg in self.sound_player.sound_dict:
-            print("Playing sound: ", msg)
-            self.sound_player.play_sound(msg)
-            sock.send("Played sound".encode('utf-8'))
+        if msg == "random_meme":
+            sock.send("Playing random meme".encode('utf-8'))
+            self.send_status("running")
+            self.sound_player.play_random_meme()
+            sock.send("Played random meme".encode('utf-8'))
+            self.send_status("idle")
         else:
-            print("Sound not found: ", msg)
-            sock.send("Sound not found".encode('utf-8'))
+            if msg in self.sound_player.sound_dict:
+                print("Playing sound: ", msg)
+                self.sound_player.play_sound(msg)
+                sock.send("Played sound".encode('utf-8'))
+            else:
+                print("Sound not found: ", msg)
+                sock.send("Sound not found".encode('utf-8'))
+
+    def send_status(self, status):
+        self.status_socket.send(status.encode('utf-8'))
 
 
 def main():
     parent_path = Path(__file__).parent.parent.absolute()
     sound_folder = parent_path / 'sound_files'
-    request_address = "ipc:///tmp/RAAI/sound_request.ipc"
+    meme_sound_folder = parent_path / 'meme_sound_files'
 
-    player = SoundPlayer(sound_folder)
-    request_handler = RequestHandler(player, request_address)
+    status_socket = pynng.Pub0()
+    status_socket.listen(STATUS_ADDRESS)
+    sleep(2)
+
+    player = SoundPlayer(sound_folder, meme_sound_folder)
+    request_handler = RequestHandler(player, REQUEST_ADDRESS, status_socket)
 
     request_handler.run()
-
-
-if __name__ == "__main__":
-    main()
--- testing/request_test.py
+++ testing/request_test.py
@@ -7,7 +7,7 @@ SOUND_REQUEST_ADDRESS = "ipc:///tmp/RAAI/sound_request.ipc"
 def send_sound_request():
     with pynng.Req0() as sock:
         sock.dial(SOUND_REQUEST_ADDRESS)
-        sound_file = 'bicycle-bell.mp3'
+        sound_file = 'random_meme'
         sock.send(sound_file.encode('utf-8'))
 
         try:
@@ -23,7 +23,7 @@ def main():
     root = tk.Tk()
     root.title("Sound Request App")
 
-    button = tk.Button(root, text="Send Sound Request", command=send_sound_request)
+    button = tk.Button(root, text="Sound", command=send_sound_request)
     button.pack(pady=10)
 
     root.mainloop()
